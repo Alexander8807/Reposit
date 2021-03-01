c5fc473d72a23f2ea473d0d8a9976707516fb040
Alexander8807 <aleksandr-petris@mail.ru>
2021-03-01 17:57:15 +0200

    Test

--- Zadanija/TableHome.py
+++ Zadanija/TableHome.py
@@ -13,25 +13,25 @@ g = "Perceptions of corruption"
 
 try:
     if ent == a:
-        a1 = df.sort_values("Score").tail(16)
+        a1 = df.sort_values("Score", ascending=False).head(16)
         a1.to_csv("TableHome1.csv", columns=["Country or region", "Score"])
     elif ent == b:
-        b1 = df.sort_values("GDP per capita").tail(16)
+        b1 = df.sort_values("GDP per capita", ascending=False).head(16)
         b1.to_csv("TableHome1.csv", columns=["Country or region", "GDP per capita"])
     elif ent == c:
-        c1 = df.sort_values("Social support").tail(16)
+        c1 = df.sort_values("Social support", ascending=False).head(16)
         c1.to_csv("TableHome1.csv", columns=["Country or region", "Social support"])
     elif ent == d:
-        d1 = df.sort_values("Healthy life expectancy").tail(16)
+        d1 = df.sort_values("Healthy life expectancy", ascending=False).head(16)
         d1.to_csv("TableHome1.csv", columns=["Country or region", "Healthy life expectancy"])
     elif ent == e:
-        e1 = df.sort_values("Freedom to make life choices").tail(16)
+        e1 = df.sort_values("Freedom to make life choices", ascending=False).head(16)
         e1.to_csv("TableHome1.csv", columns=["Country or region", "Freedom to make life choices"])
     elif ent == f:
-        f1 = df.sort_values("Generosity").tail(16)
+        f1 = df.sort_values("Generosity", ascending=False).head(16)
         f1.to_csv("TableHome1.csv", columns=["Country or region", "Generosity"])
     elif ent == g:
-        g1 = df.sort_values("Perceptions of corruption").tail(16)
+        g1 = df.sort_values("Perceptions of corruption", ascending=False).head(16)
         g1.to_csv("TableHome1.csv", columns=["Country or region", "Perceptions of corruption"])
     else:
         print("Invalid request")
